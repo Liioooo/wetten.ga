bf7568881cee58c5fce4b750c2be2ac993fde12c
Leo Preissegger <preissegger.leo@a1.net>
2019-02-11 16:53:00 +0100

    added chat

--- src/app/modules/roulette/roulette.module.ts
+++ src/app/modules/roulette/roulette.module.ts
@@ -6,9 +6,10 @@ import { RouletteComponent } from './components/roulette/roulette.component';
 import { RouletteRollerComponent } from './components/roulette-roller/roulette-roller.component';
 import {SharedModule} from '../../shared/shared.module';
 import { RollHistoryComponent } from './components/roll-history/roll-history.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
-  declarations: [RouletteComponent, RouletteRollerComponent, RollHistoryComponent],
+  declarations: [RouletteComponent, RouletteRollerComponent, RollHistoryComponent, ChatComponent],
   imports: [
       CommonModule,
       RouletteRoutingModule,
