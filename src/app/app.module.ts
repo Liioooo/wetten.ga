4e793cbd310efb3116e658c2233aa9d3d5d28391
Filip <6134@htl.rennweg.at>
2019-02-12 14:03:36 +0100

    Enabled Firestore Offline persistance Fixed lots of ngIfs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
       SharedModule.forRoot(),
       AppRoutingModule,
       AngularFireModule.initializeApp(firebaseConfig),
-      AngularFirestoreModule,
+      AngularFirestoreModule.enablePersistence(),
       AngularFireAuthModule,
       AngularFireDatabaseModule,
       BrowserAnimationsModule
--- src/app/modules/roulette/components/roll-history/roll-history.component.ts
+++ src/app/modules/roulette/components/roll-history/roll-history.component.ts
@@ -1,25 +1,46 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {RouletteService} from '../../../../shared/services/roullete/roullete.service';
 import {Roll} from '../../../../shared/models/Roll';
 import {AnimationFinishedService} from '../../services/animation-finished.service';
-import {Observable} from 'rxjs';
-import {debounce} from 'rxjs/operators';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-roll-history',
   templateUrl: './roll-history.component.html',
   styleUrls: ['./roll-history.component.scss']
 })
-export class RollHistoryComponent implements OnInit {
+export class RollHistoryComponent implements OnInit, OnDestroy {
 
-  public rollHistory$: Observable<Roll[]>;
+  public rollHistory: Roll[];
+  private rollHistoryDelayed: Roll[];
+
+  private rollHistorySubscription: Subscription;
+  private animationFinishedSubscription: Subscription;
 
   constructor(public rouletteService: RouletteService, private animationFinishedService: AnimationFinishedService) { }
 
+  private getClass(num: number): string {
+    if (num === 0) {
+      return 'bg-success';
+    } else if (num <= 7 && num > 0) {
+      return 'bg-danger';
+    } else if (num >= 8) {
+      return 'bg-dark-fix';
+    }
+    return '';
+  }
+
+  
   ngOnInit() {
-    this.rollHistory$ = this.rouletteService.rollHistory$.pipe(
-        debounce(() => this.animationFinishedService.animationFinished)
-    );
+    this.rollHistorySubscription = this.rouletteService.rollHistory$.subscribe(history => this.rollHistoryDelayed = history);
+    this.animationFinishedSubscription = this.animationFinishedService.animationFinished.subscribe(() => {
+      this.rollHistory = this.rollHistoryDelayed;
+    });
+  }
+  
+  ngOnDestroy() {
+    this.rollHistorySubscription.unsubscribe();
+    this.animationFinishedSubscription.unsubscribe();
   }
 
 }
