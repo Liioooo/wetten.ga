e32630623881d4caa89f6482a942c2c6b7b0cabf
Filip <filipcoja@gmail.com>
2019-02-18 21:45:37 +0100

    Extended Firestore rules Finalized deleting money after setting bet

--- functions/src/removeMoney.ts
+++ functions/src/removeMoney.ts
@@ -1,38 +1,53 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import {Bet} from './models/Bet';
 import {BetType} from './models/enums';
+import {Bet} from './models/Bet';
+type DocumentSnapshot = admin.firestore.DocumentSnapshot;
 
-const db = admin.firestore();
-
+// const db = admin.firestore();
 export const removeMoney = functions.firestore
-  .document('users/{userId}')
-  .onWrite(async (change, context) => {
-    if(change.after) {
-      const before = change.before;
-      const after = change.after;
-      if (before.redAmount !== after.redAmount) {
-        transform(before, after, BetType.red);
-      } else if (before.blackAmount !== after.blackAmount) {
-        transform(before, after, BetType.black);
-      } else if (before.greenAmount !== after.greenAmount) {
-        transform(before, after, BetType.green);
+  .document('bets/{userId}')
+  .onWrite(async (change: functions.Change<DocumentSnapshot>, context) => {
+    const beforeSnapshot = change.before;
+    const afterSnapshot = change.after;
+    if(beforeSnapshot) {
+      const before: Bet = beforeSnapshot.data() as Bet;
+      const after: Bet = afterSnapshot.data() as Bet;
+      if (after && before) {
+        if (before.redAmount !== after.redAmount) {
+          await transform(before, after, BetType.red);
+        } else if (before.blackAmount !== after.blackAmount) {
+          await transform(before, after, BetType.black);
+        } else if (before.greenAmount !== after.greenAmount) {
+          await transform(before, after, BetType.green);
+        }
+        return "done";
       }
-      return "done";
     }
     return "It was a delete, no action taken";
   });
 
 
-function transform(before, after, type: BetType) {
-  const amountBefore = before[type] ? before[type] : 0;
-  const diff = after[type] - amountBefore;
-  db.runTransaction(transaction => transaction.get(after.user)
-    .then(userDoc => {
-      if (!userDoc.exists) { return; }
-      const userData = userDoc.data();
-      if(!userData.amount) { return; }
-      transaction.update(userDoc.ref, {amount: userData.amount - diff});
-    }))
-    .catch(console.error);
+async function transform(before, after, type: BetType = BetType.black) {
+  if(before && after) {
+    const amountBefore = before[type] ? before[type] : 0;
+    const diff = after[type] - amountBefore;
+
+    const user = (await after.user.get()).data();
+    await after.user.update({amount: user.amount - diff})
+
+    /*
+    db.runTransaction(transaction => transaction.get(after.user as DocumentReference)
+      .then(userDoc => {
+        const exists = userDoc.exists;
+        if (!exists) { return; }
+        const userData = userDoc.data();
+        if(!userData) { return; }
+        transaction.update(userDoc.ref, {amount: userData.amount - diff});
+      }))
+      .catch(console.error);
+      */
+  }
+
+
 }
